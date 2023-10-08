c0714b3621ea387713d5b32101b668e68890b1a2
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-08 02:19:06 +0100

    feat: refactor generate salt script

--- generateSalt.ts
+++ generateSalt.ts
@@ -2,76 +2,90 @@ import * as bcrypt from "bcrypt";
 import * as fs from "fs";
 import readline from "readline";
 
-const saltRounds = 10;
-const envFilePath = ".env";
+const BCRYPT_SALT_ROUNDS = 10;
+const ENV_FILE_PATH = ".env";
+const ENV_VARIABLE_NAME = "API_KEY_SALT=";
+const READ_FILE_ENCODING = "utf-8";
 
 async function generateSalt() {
   try {
     let envContent = "";
 
-    // Check if the .env file already exists
-    if (fs.existsSync(envFilePath)) {
-      // Read the content of the .env file
-      envContent = fs.readFileSync(envFilePath, "utf-8");
-
-      // Check if API_KEY_SALT is defined and set
-      const match = envContent.match(/API_KEY_SALT=(.+)/);
-      if (match) {
-        const existingSalt = match[1].trim();
-        if (existingSalt) {
-          // Ask for confirmation before replacing the salt
-          const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-          });
-
-          rl.question(
-            "Warning: API_KEY_SALT variable is already defined and set in the .env file. Replacing it will make previously created API keys invalid. Continue? (yes/no): ",
-            async (answer) => {
-              rl.close();
-
-              if (answer.toLowerCase() !== "yes") {
-                console.log("Salt generation aborted.");
-                return;
-              }
-
-              // Replace the existing API_KEY_SALT with the new one
-              envContent = envContent.replace(
-                /API_KEY_SALT=(.+)/,
-                `API_KEY_SALT=${await generateNewSalt()}`
-              );
-              writeEnvFile(envContent);
-            }
-          );
-        } else {
-          // API_KEY_SALT is defined but not set, generate and write salt
-          const newSalt = await generateNewSalt();
-          envContent = envContent.replace(/API_KEY_SALT=.*/, `API_KEY_SALT=${newSalt}`);
-          writeEnvFile(envContent);
-        }
-      } else {
-        // API_KEY_SALT is not defined, generate and write salt
-        const newSalt = await generateNewSalt();
-        writeEnvFile(`${envContent}\nAPI_KEY_SALT=${newSalt}`);
-      }
-    } else {
-      // .env file doesn't exist, generate and write salt
-      const newSalt = await generateNewSalt();
-      writeEnvFile(`API_KEY_SALT=${newSalt}`);
+    if (!fs.existsSync(ENV_FILE_PATH)) {
+      writeSalt(`${ENV_VARIABLE_NAME}=`);
+      return;
     }
+
+    // Check if the .env file already exists
+    envContent = fs.readFileSync(ENV_FILE_PATH, READ_FILE_ENCODING);
+
+    // Check if API_KEY_SALT is defined and set
+    const match = RegExp(/API_KEY_SALT=(.+)/).exec(envContent);
+
+    if (!match) {
+      writeSalt(`${envContent}\n${ENV_VARIABLE_NAME}=`);
+      return;
+    }
+
+    const existingSalt = match[1].trim();
+
+    if (!existingSalt) {
+      // API_KEY_SALT is defined but not set, generate and write salt
+      const newSalt = await generateNewSalt();
+
+      envContent = envContent.replace(/API_KEY_SALT=.*/, `${ENV_VARIABLE_NAME}=${newSalt}`);
+
+      writeEnvFile(envContent);
+      return;
+    }
+
+    getConfirmation(async (isReplacementConfirmed) => {
+      if (!isReplacementConfirmed) {
+        return;
+      }
+
+      envContent = envContent.replace(/API_KEY_SALT=(.+)/, `${ENV_VARIABLE_NAME}=${await generateNewSalt()}`);
+
+      writeEnvFile(envContent);
+    });
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
+async function writeSalt(text: string) {
+  const newSalt = await generateNewSalt();
+  writeEnvFile(`${text}${newSalt}`);
+}
+
+function getConfirmation(onConfirm: (confirmation: boolean) => Promise<void>) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question(
+    "Warning: API_KEY_SALT variable is already defined and set in the .env file. Replacing it will make previously created API keys invalid. Continue? (yes/no): ",
+    (answer) => {
+      rl.close();
+
+      if (answer.toLowerCase() === "yes") {
+        onConfirm(true);
+      } else {
+        onConfirm(false);
+      }
+    }
+  );
+}
+
 async function generateNewSalt() {
   // Generate a new salt using bcrypt
-  return await bcrypt.genSalt(saltRounds);
+  return await bcrypt.genSalt(BCRYPT_SALT_ROUNDS);
 }
 
 function writeEnvFile(content: string) {
   // Create or update the .env file with the new content
-  fs.writeFileSync(envFilePath, content);
+  fs.writeFileSync(ENV_FILE_PATH, content);
 
   console.log("Salt generated and .env file updated successfully.");
 }
