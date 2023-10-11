911fa5f772b95590fe110818ab3fdc849ad4ed3e
chukwuemekasantos <chukwuanthony544@gmail.com>
2023-10-11 06:15:11 +0100

    fixed route chaining

--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,9 @@ import projectRouter from "./Project/ProjectRoutes";
 
 const appRouter = express.Router();
 
-appRouter.use("/auth", apiAuthRouter);
-appRouter.use("/projects", projectRouter);
+appRouter
+    .use("/auth", apiAuthRouter)
+    .use("/projects", projectRouter);
 // Todo: Add other routes
 
 export default appRouter;
